408d0b522b6f8420b3adee9f8817e896a6ec4ce2
Ceylo <lucas.soltic@orange.fr>
2021-10-17 21:58:20 +0200

    Next SwiftUI project

---
+++ FurAffinity/FurAffinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurAffinityApp.swift
+//  FurAffinity
+//
+//  Created by Ceylo on 17/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FurAffinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
