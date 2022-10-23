c9294700d8964361857e1f0dcf632c33ff0d9361
Ceylo <lucas.soltic@orange.fr>
2022-10-23 08:52:19 +0200

    Use more relative queries

--- FAKit/Sources/FAPages/FASubmissionPage.swift
+++ FAKit/Sources/FAPages/FASubmissionPage.swift
@@ -28,8 +28,8 @@ extension FASubmissionPage {
             
             let doc = try SwiftSoup.parse(String(decoding: data, as: UTF8.self))
             let submissionContentQuery = "body div#main-window div#site-content div#submission_page div#columnpage div.submission-content"
-            let imageQuery = submissionContentQuery + " div.submission-area img#submissionImg"
-            let submissionImgNode = try doc.select(imageQuery)
+            let submissionContentNode = try doc.select(submissionContentQuery)
+            let submissionImgNode = try submissionContentNode.select("div.submission-area img#submissionImg")
             
             let previewStr = try submissionImgNode.attr("data-preview-src")
                 .addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
@@ -43,8 +43,7 @@ extension FASubmissionPage {
             self.previewImageUrl = previewUrl
             self.fullResolutionImageUrl = fullViewUrl
             
-            let favoriteQuery = submissionContentQuery + " div.favorite-nav a.button"
-            let favoriteNode = try doc.select(favoriteQuery)
+            let favoriteNode = try submissionContentNode.select("div.favorite-nav a.button")
             let favoriteUrlNode = try favoriteNode
                 .first(where: { ["+Fav", "-Fav"].contains(try $0.text()) })
                 .unwrap()
@@ -54,26 +53,25 @@ extension FASubmissionPage {
             self.favoriteUrl = try URL(string: "https://www.furaffinity.net" + favoriteUrlStr).unwrap()
             self.isFavorite = favoriteStatusStr == "-Fav"
             
-            let avatarQuery = submissionContentQuery + " section div.section-header div.submission-id-container div.submission-id-avatar img.avatar"
-            let avatarNode = try doc.select(avatarQuery)
+            let avatarQuery = "section div.section-header div.submission-id-container div.submission-id-avatar img.avatar"
+            let avatarNode = try submissionContentNode.select(avatarQuery)
             let avatarStr = try avatarNode.attr("src")
             let avatarUrl = try URL(string: "https:" + avatarStr).unwrap()
             
             self.authorAvatarUrl = avatarUrl
             
-            let submissionContainerQuery = submissionContentQuery + " section div.section-header div.submission-id-container div.submission-id-sub-container"
-            let submissionTitleQuery = submissionContainerQuery + " div.submission-title h2 p"
-            let titleNode = try doc.select(submissionTitleQuery)
+            let submissionContainerQuery = "section div.section-header div.submission-id-container div.submission-id-sub-container"
+            let submissionContainerNode = try submissionContentNode.select(submissionContainerQuery)
+            let titleNode = try submissionContainerNode.select("div.submission-title h2 p")
             self.title = try titleNode.text()
             
-            let authorQuery = submissionContainerQuery + " a"
-            let authorNode = try doc.select(authorQuery).first().unwrap()
+            let authorNode = try submissionContainerNode.select("a").first().unwrap()
             let authorUrl = try authorNode.attr("href")
             self.author = try authorUrl.substring(matching: "/user/(.+)/").unwrap()
             self.displayAuthor = try authorNode.text()
             
-            let descriptionQuery = submissionContentQuery + " section div.section-body div.submission-description"
-            let descriptionNode = try doc.select(descriptionQuery)
+            let descriptionQuery = "section div.section-body div.submission-description"
+            let descriptionNode = try submissionContentNode.select(descriptionQuery)
             let htmlContent = try descriptionNode.html()
             self.htmlDescription = htmlContent
         } catch {
