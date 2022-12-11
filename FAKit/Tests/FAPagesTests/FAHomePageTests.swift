683da0218b92eba7bf565f07157381b9c97447f7
Ceylo <lucas.soltic@orange.fr>
2022-12-11 13:35:21 +0100

    Update data for home page parsing test

--- FAKit/Tests/FAPagesTests/FAHomePageTests.swift
+++ FAKit/Tests/FAPagesTests/FAHomePageTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class FAHomePageTests: XCTestCase {
     func testLoggedOutPage_parsedAsExpected() throws {
-        let data = testData("www.furaffinity.net-loggedout.html")
+        let data = testData("www.furaffinity.net:loggedout.html")
         let page = FAHomePage(data: data)
         XCTAssertNotNil(page)
         XCTAssertNil(page?.username)
@@ -20,12 +20,12 @@ final class FAHomePageTests: XCTestCase {
     }
     
     func testLoggedInPage_parsedAsExpected() throws {
-        let data = testData("www.furaffinity.net-loggedin.html")
+        let data = testData("www.furaffinity.net:loggedin.html")
         let page = FAHomePage(data: data)
         XCTAssertNotNil(page)
         XCTAssertEqual(page?.username, "furrycount")
         XCTAssertEqual(page?.displayUsername, "Furrycount")
-        XCTAssertEqual(page?.submissionsCount, 1514)
-        XCTAssertEqual(page?.journalsCount, 193)
+        XCTAssertEqual(page?.submissionsCount, 2487)
+        XCTAssertEqual(page?.journalsCount, 308)
     }
 }
