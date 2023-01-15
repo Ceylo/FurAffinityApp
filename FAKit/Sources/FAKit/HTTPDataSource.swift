a69518177ea7bef6e44a363be073491f8a2b996a
Ceylo <lucas.soltic@orange.fr>
2023-01-15 15:54:51 +0100

    Support HTTP POST in addition to GET, used APIs require iOS 16

--- FAKit/Sources/FAKit/HTTPDataSource.swift
+++ FAKit/Sources/FAKit/HTTPDataSource.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+public enum HTTPMethod: String, CustomStringConvertible {
+    public var description: String { rawValue }
+    
+    case GET
+    case POST
+}
+
 public protocol HTTPDataSource {
-    func httpData(from url: URL, cookies: [HTTPCookie]?) async -> Data?
+    func httpData(from url: URL, cookies: [HTTPCookie]?,
+                  method: HTTPMethod,
+                  parameters: [URLQueryItem]) async -> Data?
+}
+
+public extension HTTPDataSource {
+    func httpData(from url: URL, cookies: [HTTPCookie]?) async -> Data? {
+        await httpData(from: url, cookies: cookies, method: .GET, parameters: [])
+    }
 }
