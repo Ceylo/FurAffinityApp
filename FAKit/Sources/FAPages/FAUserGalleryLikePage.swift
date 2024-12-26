ce985a2e582edcaf8fab3e434230c12e704a8957
Ceylo <lucas.soltic@orange.fr>
2024-12-26 13:09:36 +0100

    Support for parsing folders tree

--- FAKit/Sources/FAPages/FAUserGalleryLikePage.swift
+++ FAKit/Sources/FAPages/FAUserGalleryLikePage.swift
@@ -8,14 +8,20 @@
 import Foundation
 @preconcurrency import SwiftSoup
 
+public enum FAFolderItem: Sendable, Equatable {
+    case section(title: String)
+    case folder(title: String, url: URL)
+}
+
 public struct FAUserGalleryLikePage: Sendable {
     public let previews: [FASubmissionsPage.Submission?]
     public let displayAuthor: String
     public let nextPageUrl: URL?
+    public let folderItems: [FAFolderItem]
 }
 
 extension FAUserGalleryLikePage {
-    public init?(data: Data) async {
+    public init?(data: Data, url: URL) async {
         let state = signposter.beginInterval("User Gallery Parsing")
         defer { signposter.endInterval("User Gallery Parsing", state) }
         
@@ -48,9 +54,56 @@ extension FAUserGalleryLikePage {
             } else {
                 self.nextPageUrl = nil
             }
+            
+            let folderItemsQuery = "div#page-galleryscraps div#columnpage div.sidebar div.folder-list div.user-folders"
+            let userItems = try siteContentNode.select(folderItemsQuery)
+            self.folderItems = try Self.parseFolderItems(from: userItems, currentUrl: url)
         } catch {
             logger.error("\(#file, privacy: .public) - \(error, privacy: .public)")
             return nil
         }
+    }
+    
+    static func parseFolderItems(from node: SwiftSoup.Elements, currentUrl: URL) throws -> [FAFolderItem] {
+        enum Error: Swift.Error {
+            case unexpectedStructure
+        }
+        
+        guard node.count == 1 else {
+            if node.count == 0 {
+                return []
+            } else {
+                throw Error.unexpectedStructure
+            }
+        }
+        
+        func parseFolder(in liNode: SwiftSoup.Element) throws -> FAFolderItem {
+            let title = try liNode.text()
+            let url: URL
+            if let linkNode = try? liNode.select("a").first() {
+                let urlStr = try linkNode.attr("href")
+                url = try URL(string: FAURLs.homeUrl.absoluteString + urlStr).unwrap()
+            } else {
+                url = currentUrl
+            }
+            return .folder(title: title, url: url)
+        }
+        
+        var folderItems = [FAFolderItem]()
+        for child in node[0].children() {
+            if child.tagName() == "div" && child.hasClass("container-item-top") {
+                let title = try child.text()
+                folderItems.append(.section(title: title))
+            } else if child.tagName() == "div" && child.hasClass("default-folders") {
+                folderItems.append(contentsOf: try child.select("li").map(parseFolder))
+            } else if child.tagName() == "ul" {
+                folderItems.append(contentsOf: try child.select("li").map(parseFolder))
+            } else {
+                let html = (try? child.html()) ?? ""
+                logger.error("Unhandled tag \(child.tagName(), privacy: .public) in \(html, privacy: .public)")
+                throw Error.unexpectedStructure
+            }
+        }
+        return folderItems
     }
 }
--- FAKit/Tests/FAPagesTests/FAUserGalleryLikePageTests.swift
+++ FAKit/Tests/FAPagesTests/FAUserGalleryLikePageTests.swift
@@ -9,9 +9,51 @@ import XCTest
 @testable import FAPages
 
 final class FAUserGalleryLikePageTests: XCTestCase {
+    let tiaamaitoFolders: [FAFolderItem] = [
+        .section(title: "Gallery Folders"),
+        .folder(title: "❯❯ Main Gallery", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/")!),
+        .folder(title: "Scraps", url: URL(string: "https://www.furaffinity.net/scraps/tiaamaito/")!),
+        .section(title: "Personal"),
+        .folder(title: "Chuvareu", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147920/Chuvareu")!),
+        .folder(title: "Chuvareu Comic (archieved)", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/292599/Chuvareu-Comic-archieved")!),
+        .folder(title: "Bakemono Family", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/418988/Bakemono-Family")!),
+        .folder(title: "chars as animals", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/456419/chars-as-animals")!),
+        .folder(title: "the tiniest lord", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/566887/the-tiniest-lord")!),
+        .folder(title: "Ribbon Pooch & Co", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/566888/Ribbon-Pooch-Co")!),
+        .folder(title: "Kijani", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/566890/Kijani")!),
+        .folder(title: "Digital Pack", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/566891/Digital-Pack")!),
+        .section(title: "Closed Species"),
+        .folder(title: "Sushi Dogs", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/566883/Sushi-Dogs")!),
+        .folder(title: "Griffia", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/566884/Griffia")!),
+        .folder(title: "Memory Keepers", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/720355/Memory-Keepers")!),
+        .section(title: "for Sale"),
+        .folder(title: "P2U", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/473101/P2U")!),
+        .folder(title: "Adopts", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/473103/Adopts")!),
+        .folder(title: "Traditional Pieces", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/495402/Traditional-Pieces")!),
+        .folder(title: "Other", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/547857/Other")!),
+        .folder(title: "Art Prints", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/684620/Art-Prints")!),
+        .section(title: "Patreon"),
+        .folder(title: "2016", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/135452/2016")!),
+        .folder(title: "2017", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/297815/2017")!),
+        .folder(title: "2018", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/459438/2018")!),
+        .folder(title: "2019", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/613283/2019")!),
+        .folder(title: "2020", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/754413/2020")!),
+        .folder(title: "2021", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/937613/2021")!),
+        .section(title: "Commissions"),
+        .folder(title: "standard (cell shading)", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147923/standard-cell-shading")!),
+        .folder(title: "clear (soft shading)", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147924/clear-soft-shading")!),
+        .folder(title: "basic (base colors)", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147925/basic-base-colors")!),
+        .folder(title: "reference sheet", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147927/reference-sheet")!),
+        .folder(title: "YCH", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147928/YCH")!),
+        .folder(title: "telegram stickers", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/415109/telegram-stickers")!),
+        .folder(title: "special", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/578261/special")!),
+        .folder(title: "old works", url: URL(string: "https://www.furaffinity.net/gallery/tiaamaito/folder/147929/old-works")!),
+    ]
+    
     func testFirstGalleryPage_72itemsParsed() async throws {
+        let url = URL(string: "https://www.furaffinity.net/gallery/tiaamaito/")!
         let data = testData("www.furaffinity.net:gallery:tiaamaito:.html")
-        let asyncPage = await FAUserGalleryLikePage(data: data)
+        let asyncPage = await FAUserGalleryLikePage(data: data, url: url)
         let page = try XCTUnwrap(asyncPage)
         XCTAssertEqual(page.previews.count, 72)
         XCTAssertEqual(page.displayAuthor, "tiaamaito")
@@ -30,20 +72,24 @@ final class FAUserGalleryLikePageTests: XCTestCase {
             URL(string: "https://www.furaffinity.net/gallery/tiaamaito/2/")!,
             page.nextPageUrl
         )
+        XCTAssertEqual(tiaamaitoFolders, page.folderItems)
     }
     
     func testEmptyGalleryPage_parsedWithNoContent() async throws {
+        let url = URL(string: "https://www.furaffinity.net/gallery/furrycount/")!
         let data = testData("www.furaffinity.net:gallery:furrycount:.html")
-        let asyncPage = await FAUserGalleryLikePage(data: data)
+        let asyncPage = await FAUserGalleryLikePage(data: data, url: url)
         let page = try XCTUnwrap(asyncPage)
         XCTAssertEqual(page.previews.count, 0)
         XCTAssertEqual(page.displayAuthor, "Furrycount")
         XCTAssertEqual(page.nextPageUrl, nil)
+        XCTAssertEqual(page.folderItems, [])
     }
     
     func testScrapsPage_72itemsParsed() async throws {
+        let url = URL(string: "https://www.furaffinity.net/scraps/tiaamaito/")!
         let data = testData("www.furaffinity.net:scraps:tiaamaito:.html")
-        let asyncPage = await FAUserGalleryLikePage(data: data)
+        let asyncPage = await FAUserGalleryLikePage(data: data, url: url)
         let page = try XCTUnwrap(asyncPage)
         XCTAssertEqual(page.previews.count, 72)
         XCTAssertEqual(page.displayAuthor, "tiaamaito")
@@ -62,11 +108,13 @@ final class FAUserGalleryLikePageTests: XCTestCase {
             URL(string: "https://www.furaffinity.net/scraps/tiaamaito/2/")!,
             page.nextPageUrl
         )
+        XCTAssertEqual(page.folderItems.count, 38)
     }
     
     func testFavoritesPage_72itemsParsed() async throws {
+        let url = URL(string: "https://www.furaffinity.net/favorites/tiaamaito/")!
         let data = testData("www.furaffinity.net:favorites:tiaamaito:.html")
-        let asyncPage = await FAUserGalleryLikePage(data: data)
+        let asyncPage = await FAUserGalleryLikePage(data: data, url: url)
         let page = try XCTUnwrap(asyncPage)
         XCTAssertEqual(page.previews.count, 72)
         XCTAssertEqual(page.displayAuthor, "tiaamaito")
@@ -85,5 +133,6 @@ final class FAUserGalleryLikePageTests: XCTestCase {
             URL(string: "https://www.furaffinity.net/favorites/tiaamaito/1523944993/next")!,
             page.nextPageUrl
         )
+        XCTAssertEqual(page.folderItems, [])
     }
 }
