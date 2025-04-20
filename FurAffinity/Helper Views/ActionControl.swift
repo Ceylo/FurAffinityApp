ec96a9e43e7825de86c2b023e4c3d620379bc45f
Ceylo <lucas.soltic@orange.fr>
2025-04-20 19:21:17 +0200

    Revisit ActionControl design

--- FurAffinity/Helper Views/ActionControl.swift
+++ FurAffinity/Helper Views/ActionControl.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ActionControl: View {
     var body: some View {
-        Text("…")
-            .font(.headline)
-            .foregroundColor(.primary)
-            .padding(7.5)
-            .offset(y: -4)
+        Image(systemName: "ellipsis")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 18, height: 18)
+            .foregroundColor(.accentColor)
+            .padding(5)
             .background(.thinMaterial)
             .clipShape(Circle())
     }
 }
 
 #Preview {
-    ActionControl()
+    NavigationStack {
+        Text("")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        
+                    } label: {
+                        ActionControl()
+                    }
+                }
+            }
+    }
 }
