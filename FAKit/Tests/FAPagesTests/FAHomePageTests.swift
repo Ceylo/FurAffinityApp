a9bdaef3bf13084f39c357d78694927180850a36
Ceylo <lucas.soltic@orange.fr>
2025-10-05 14:33:50 +0200

    Remove leftover awaits

--- FAKit/Tests/FAPagesTests/FAHomePageTests.swift
+++ FAKit/Tests/FAPagesTests/FAHomePageTests.swift
@@ -9,15 +9,15 @@ import XCTest
 @testable import FAPages
 
 final class FAHomePageTests: XCTestCase {
-    func testLoggedOutPage_parsedAsExpected() async throws {
+    func testLoggedOutPage_parsedAsExpected() {
         let data = testData("www.furaffinity.net:loggedout.html")
-        let page = await FAHomePage(data: data, baseUri: FAURLs.homeUrl)
+        let page = FAHomePage(data: data, baseUri: FAURLs.homeUrl)
         XCTAssertNil(page)
     }
     
-    func testLoggedInPage_parsedAsExpected() async throws {
+    func testLoggedInPage_parsedAsExpected() throws {
         let data = testData("www.furaffinity.net:loggedin.html")
-        let page = try await FAHomePage(data: data, baseUri: FAURLs.homeUrl).unwrap()
+        let page = try FAHomePage(data: data, baseUri: FAURLs.homeUrl).unwrap()
         
         XCTAssertEqual(page, .init(
             username: "furrycount",
