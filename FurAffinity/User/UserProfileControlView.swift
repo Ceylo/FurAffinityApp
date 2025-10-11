d4152661ea7ee7608ca6fd4940ed0e800ae3910a
Ceylo <lucas.soltic@orange.fr>
2025-10-12 00:23:51 +0200

    Liquid glass for user pages

--- FurAffinity/User/UserProfileControlView.swift
+++ FurAffinity/User/UserProfileControlView.swift
@@ -11,18 +11,34 @@ struct UserProfileControlView: View {
     var username: String
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 0) {
-                ForEach(UserProfileControl.allCases) { control in
-                    Link(destination: control.destinationUrl(for: username)) {
-                        Text(control.title)
-                            .font(.headline)
-                            .padding(15)
+        if #available(iOS 26, *) {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(UserProfileControl.allCases) { control in
+                        Link(destination: control.destinationUrl(for: username)) {
+                            Text(control.title)
+                                .font(.headline)
+                                .padding(5)
+                        }
+                        .buttonStyle(.glass)
+                    }
+                }
+                .padding(5)
+            }
+        } else {
+            ScrollView(.horizontal) {
+                HStack(spacing: 0) {
+                    ForEach(UserProfileControl.allCases) { control in
+                        Link(destination: control.destinationUrl(for: username)) {
+                            Text(control.title)
+                                .font(.headline)
+                                .padding(15)
+                        }
                     }
                 }
             }
+            .background(.regularMaterial)
         }
-        .background(.regularMaterial)
     }
 }
 
