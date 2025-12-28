e768acaea4852b3b62a3f170236281c4fc5afd13
Ceylo <lucas.soltic@orange.fr>
2025-12-28 15:48:09 +0100

    Add checkerboard background in notification overlay preview

--- FurAffinity/Helper Views/NotificationOverlay.swift
+++ FurAffinity/Helper Views/NotificationOverlay.swift
@@ -69,8 +69,37 @@ struct NotificationOverlay: View {
     }
 }
 
+private struct Checkerboard: Shape {
+    let rows: Int
+    let columns: Int
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0 ..< rows {
+            for column in 0 ..< columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
 #Preview(traits: .sizeThatFitsLayout) {
     NotificationOverlay(itemCount: .constant(12))
         .padding()
-        .background(.yellow)
+        .background(Checkerboard(rows: 5, columns: 16).fill(.cyan))
 }
