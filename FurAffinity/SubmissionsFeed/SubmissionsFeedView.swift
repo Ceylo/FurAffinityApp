c9348a4bcf37f4627546720bd48db7c97d1a64d5
Ceylo <lucas.soltic@orange.fr>
2026-01-17 20:10:17 +0100

    Fix thumbnails preloading on submissions feed

--- FurAffinity/SubmissionsFeed/SubmissionsFeedView.swift
+++ FurAffinity/SubmissionsFeed/SubmissionsFeedView.swift
@@ -163,7 +163,7 @@ struct SubmissionsFeedView: View {
                 .swap(when: items.isEmpty) {
                     noPreview
                 }
-                .onChange(of: model.submissionPreviews) { _, newValue in
+                .onChange(of: model.submissionPreviews, initial: true) { _, newValue in
                     guard let previews = newValue else { return }
                     prefetchThumbnails(for: previews, availableWidth: geometry.size.width)
                     prefetchAvatars(for: previews)
